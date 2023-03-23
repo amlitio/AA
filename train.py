70690b58b297a8d0277d3f4ac0a563e1788e9665
kleos <36254493+amlitio@users.noreply.github.com>
2023-03-23 02:26:00 -0400

    Create train.py

---
+++ train.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Define the LLM
+class LLM(nn.Module):
+  def __init__(self, input_size, output_size):
+    super().__init__()
+    self.fc1 = nn.Linear(input_size, 1024)
+    self.fc2 = nn.Linear(1024, output_size)
+
+  def forward(self, x):
+    x = self.fc1(x)
+    x = self.fc2(x)
+    return x
+
+# Define the game
+class Game:
+  def __init__(self, state):
+    self.state = state
+    self.actions = [
+      "Raise",
+      "Call",
+      "Fold",
+      "Check",
+      "Bet",
+    ]
+    self.rewards = [
+      -1,
+      0,
+      1,
+      -1,
+      0,
+    ]
+
+  def take_action(self, action):
+    self.state, reward, done = self.step(action)
+    return reward, done
+
+  def step(self, action):
+    # Take the action
+    self.state = self.state.next(action)
+    # Get the reward
+    reward = self.rewards[self.state]
+    # Get the done signal
+    done = reward == -1 or self.state == "End"
+    return reward, done
+
+# Define the training process
+def train(llm, game):
+  # Initialize the LLM
+  llm.load_state_dict("./saved_model")
+
+  # Initialize the optimizer
+  optimizer = optim.Adam(llm.parameters())
+
+  # Initialize the episode counter
+  episode_counter = 0
+
+  # Initialize the total reward
+  total_reward = 0
+
+  # Initialize the done signal
+  done = False
+
+  while not done:
+    # Get the current state
+    state = game.state
+
+    # Get the action
+    action, _ = llm(state)
+
+    # Take the action
+    game.take_action(action)
+
+    # Get the reward and done signal
+    reward, done = game.step(action)
+
+    # Update the LLM
+    total_reward += reward
+    llm.zero_grad()
+    loss = -reward
+    loss.backward()
+    optimizer.step()
+
+    # Increment the episode counter
+    episode_counter += 1
+
+  # Print the total reward
+  print("Total reward:", total_reward)
