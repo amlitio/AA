d4bc622cb49c4a142df0aa2583395d917c5318c2
kleos <36254493+amlitio@users.noreply.github.com>
2023-03-23 14:31:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Define the LLM
+class LLM(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(LLM, self).__init__()
+        self.fc1 = nn.Linear(input_size, 1024)
+        self.fc2 = nn.Linear(1024, output_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Define the Game, State, and train function as in the original code snippet
+
+# Then create a main function to run the training process
 def main():
-   input_size = 4  # adjust number according to state of dimension
-   output_size = 5  # number of
+    input_size = 5  # Set input size according to your problem
+    output_size = 5  # Set output size according to your problem
+    initial_state = "Start"  # Set initial state according to your problem
+
+    # Initialize the LLM and game
+    llm = LLM(input_size, output_size)
+    game = Game(initial_state)
+
+    # Train the LLM
+    train(llm, game)
+
+    # Save the trained model
+    torch.save(llm.state_dict(), "./saved_model.pt")
+
+if __name__ == "__main__":
+    main()
+
+
+#Remember to include the Game, State, and train functions from your original code snippet, 
